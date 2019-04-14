e1c4451cdd1611949f18f2967ee6923c91fa8086
yakitori <>
2019-04-14 18:37:09 +0900

    no message

--- Runtime/ScatterComponent.cs
+++ Runtime/ScatterComponent.cs
@@ -3,45 +3,45 @@ using UniRx;
 using UnityEngine;
 
 namespace RetroLevel {
-	[ExecuteInEditMode, SelectionBase, DisallowMultipleComponent, RequireComponent(typeof(BoxCollider))]
-	public class ScatterComponent : MonoBehaviour {
-		////// Near-Constructors //////
+  [ExecuteInEditMode, SelectionBase, DisallowMultipleComponent, RequireComponent(typeof(BoxCollider))]
+  public class ScatterComponent : MonoBehaviour {
+    ////// Near-Constructors //////
 
-		////// Props //////
-		[SerializeField] private GameObject prefab = null;
-		[SerializeField] private uint amount = 10;
+    ////// Props //////
+    [SerializeField] private GameObject prefab = null;
+    [SerializeField] private uint amount = 10;
 
-		////// Methods //////
-		protected void OnValidate() => this.Generate();
-		// protected void OnEnable() => this.Generate();
-		// protected void OnDisable() => this.Clear();
+    ////// Methods //////
+    protected void OnValidate() => this.Generate();
+    // protected void OnEnable() => this.Generate();
+    // protected void OnDisable() => this.Clear();
 
-		private void Clear() {
-			foreach (var child in this.transform.Cast<Transform>().ToArray()) {
+    private void Clear() {
+      foreach (var child in this.transform.Cast<Transform>().ToArray()) {
         Object.DestroyImmediate(child.gameObject);
-			}
-		}
+      }
+    }
 
-		private void Generate() {
-			if (this.prefab == null) { return; }
+    private void Generate() {
+      if (this.prefab == null) { return; }
 
-			this.gameObject.name = "Scatter: [" + this.prefab.name + "]";
-			var boxCollider = this.GetComponent<BoxCollider>();
-			boxCollider.enabled = false;
+      this.gameObject.name = "Scatter: [" + this.prefab.name + "]";
+      var boxCollider = this.GetComponent<BoxCollider>();
+      boxCollider.enabled = false;
 
-			Observable.NextFrame().Subscribe(_ => {
-				this.Clear();
+      Observable.NextFrame().Subscribe(_ => {
+        this.Clear();
 
-				var pos = Vector3.zero;
-				foreach (var __ in Enumerable.Range(0, (int)this.amount)) {
-           var go = Object.Instantiate(this.prefab, this.transform, false);
-           var size = boxCollider.size * 0.5f;
-           pos.x = Random.Range(-size.x, size.x);
-           pos.y = Random.Range(-size.y, size.y);
-           pos.z = Random.Range(-size.z, size.z);
-           go.transform.localPosition = pos;
-				}
+        var pos = Vector3.zero;
+        foreach (var __ in Enumerable.Range(0, (int) this.amount)) {
+          var go = Object.Instantiate(this.prefab, this.transform, false);
+          var size = boxCollider.size * 0.5f;
+          pos.x = Random.Range(-size.x, size.x);
+          pos.y = Random.Range(-size.y, size.y);
+          pos.z = Random.Range(-size.z, size.z);
+          go.transform.localPosition = pos;
+        }
       });
-		}
-	}
+    }
+  }
 }
