3cf52bbc75b95401f6bbca5d351a5cfab9ec35d7
yakitori <>
2019-04-14 17:54:27 +0900

    no message

--- Runtime/Utils/LinqExtensions.cs
+++ Runtime/Utils/LinqExtensions.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using Random = UnityEngine.Random;
 
 namespace RetroLevel {
-  public static class LinqExtension {
+  public static class LinqExtensions {
     public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, System.Action<T> action) {
       foreach(T item in source) action(item);
       return source;
