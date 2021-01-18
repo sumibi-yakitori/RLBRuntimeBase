de3034b08b1e96f1ea902d00582d0d20a656200c
sumibi-yakitori <nekosoft360@gmail.com>
2021-01-18 16:15:28 +0900

    no message

--- Runtime/Utils/LinqExtensions.cs
+++ Runtime/Utils/LinqExtensions.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Random = UnityEngine.Random;
 
-namespace RLB {
+namespace Bloc {
   public static class LinqExtensions {
     public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, System.Action<T> action) {
       foreach(T item in source) action(item);
